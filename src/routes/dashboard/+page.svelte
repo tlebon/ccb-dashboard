bab199666c097808841b09fbcaac30cf9d735502
Timothy Le Bon <tlebon@gmail.com>
2025-06-05 00:55:13 +0200

    feat: add dashboard

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import ThisWeek from '../+page.svelte';
+  import NextWeek from '../next-week/+page.svelte';
+  import { onMount, onDestroy } from 'svelte';
+  import { fade } from 'svelte/transition';
+
+  let showNextWeek = false;
+  let interval: ReturnType<typeof setInterval>;
+
+  // Set your desired rotation time (e.g., 30 seconds)
+  const ROTATE_MS = 30000;
+
+  onMount(() => {
+    interval = setInterval(() => {
+      showNextWeek = !showNextWeek;
+    }, ROTATE_MS);
+  });
+
+  onDestroy(() => {
+    clearInterval(interval);
+  });
+</script>
+
+{#if showNextWeek}
+  <div transition:fade><NextWeek /></div>
+{:else}
+  <div transition:fade><ThisWeek /></div>
+{/if} 
